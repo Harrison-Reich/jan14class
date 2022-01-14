8da8b7716a80d7d0b2c37780d43cfe61e4fbb740
Harrison Reich <hreich1998@gmail.com>
2022-01-14 11:09:11 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// sugar syntax (looks nicer and easer to write)
+// both codes do the same
+const App = () => {
+  return(
+  <>
+    <h1>Hello World!</h1>
+    <button>Click Me</button>
+  </>
+  )
 }
+// React.createElement('h1', 'Hello Word!')
 
-export default App;
+export default App
